523a8e4383a084954087d88bfb7be65f8dd57e7f
sedasnglu <62130541+sedasnglu@users.noreply.github.com>
2022-09-27 16:29:33 +0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+        fmt.Fprintf(w, "Hello World", r.URL.Path)
     })
     http.ListenAndServe(":9009", nil)
 }
