baac5971f6e04de2d2d47149b1d24f1ca978204a
sedasnglu <62130541+sedasnglu@users.noreply.github.com>
2022-09-30 09:24:44 +0300

    Create hello.go

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello Huawei!")
+        fmt.Fprintf(w, "Hello World!")
     })
     http.ListenAndServe(":9009", nil)
 }
